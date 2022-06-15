60ed5f6ed6c527a4df98411be6e1f3c8ed6953cf
madelinemarshall <29718995+madelinemarshall@users.noreply.github.com>
2022-06-15 12:18:32 +1000

    Adjust testing file so passes

--- test_plot_galaxies.py
+++ test_plot_galaxies.py
@@ -4,7 +4,7 @@ from matplotlib import rcParams
 import pandas as pd
 from astropy.io import fits
 import pickle
-import mock_functions
+import bluetides_mock_catalogue_functions as mock_functions
 
 
 
@@ -15,7 +15,8 @@ filterList=[('euclid','nisp','y'),('hst','wfc3','f105w'),
 
 def test_invalid_filter():
     z=11
-    cat=mock_functions.Catalogue(z=z,catalogue_path='../')
+    #cat=mock_functions.Catalogue(z=z,image_path='../') ##NOTE: if running in a directory other than where the catalogue is saved, will need to specify an image_path
+    cat=mock_functions.Catalogue(z=z)
     cat.select_galaxies_with_constraints(stellar_mass_constraint=(9.2,9.5))#z11
 
     out = cat.plot_galaxies('jwst','NIRCam','f444m')
@@ -24,7 +25,7 @@ def test_invalid_filter():
 
 def test_invalid_jwst_instrument():
     z=11
-    cat=mock_functions.Catalogue(z=z,catalogue_path='../')
+    cat=mock_functions.Catalogue(z=z)
     cat.select_galaxies_with_constraints(stellar_mass_constraint=(9.2,9.5))#z11
     out = cat.plot_galaxies('jwst','mn','F444W')
     assert out =='ERROR: Specified jwst instrument "mn" is not nircam, miri, or all, aborted'
@@ -32,7 +33,7 @@ def test_invalid_jwst_instrument():
     
 def test_invalid_instrument():
     z=11
-    cat=mock_functions.Catalogue(z=z,catalogue_path='../')
+    cat=mock_functions.Catalogue(z=z)
     cat.select_galaxies_with_constraints(stellar_mass_constraint=(9.2,9.5))#z11
     cat.plot_galaxies('euclid','INVALID','y') 
     #Should correct itself and run as normal
@@ -40,7 +41,7 @@ def test_invalid_instrument():
 
 def test_invalid_telescope():
     z=11
-    cat=mock_functions.Catalogue(z=z,catalogue_path='../')
+    cat=mock_functions.Catalogue(z=z)
     cat.select_galaxies_with_constraints(stellar_mass_constraint=(9.2,9.5))#z11
     out = cat.plot_galaxies('Spitzer','NIRCam','F444M')
     assert out == "ERROR: Telescope 'spitzer' doesn't match those available ('hst','jwst','euclid','roman','vista','subaru')"
@@ -48,7 +49,7 @@ def test_invalid_telescope():
     
 def test_z12_two_galaxies_one_filter():
     z=12
-    cat=mock_functions.Catalogue(z=z,catalogue_path='../')
+    cat=mock_functions.Catalogue(z=z)
     cat.select_galaxies_with_indices([1,2])
     cat.plot_galaxies('vista','vircam','y')
     plt.close('all')
@@ -57,7 +58,7 @@ def test_z12_two_galaxies_one_filter():
     
 def test_z12_all_telescopes_all_filters():
     z=12
-    cat=mock_functions.Catalogue(z=z,catalogue_path='../')
+    cat=mock_functions.Catalogue(z=z)
     cat.select_galaxies_with_indices([1,2])
     cat.plot_galaxies('all','all','all')
     plt.close('all')
@@ -66,7 +67,7 @@ def test_z12_all_telescopes_all_filters():
     
 def test_z12_all_telescopes_Y():
     z=12
-    cat=mock_functions.Catalogue(z=z,catalogue_path='../')
+    cat=mock_functions.Catalogue(z=z)
     cat.select_galaxies_with_indices([1,2])
     cat.plot_galaxies('all','all','y')
     plt.close('all')
@@ -75,7 +76,7 @@ def test_z12_all_telescopes_Y():
     
 def test_z12_one_galaxy():
     z=12
-    cat=mock_functions.Catalogue(z=z,catalogue_path='../')
+    cat=mock_functions.Catalogue(z=z)
     cat.select_galaxies_with_indices([1])
     cat.plot_galaxies('vista','vircam','all')
     cat.plot_galaxies('euclid','nisp','all')
@@ -94,7 +95,7 @@ def test_z12_one_galaxy():
 
 def test_z11():
     z=11
-    cat=mock_functions.Catalogue(z=z,catalogue_path='../')
+    cat=mock_functions.Catalogue(z=z)
     cat.select_galaxies_with_indices([1])
     cat.plot_galaxies('vista','vircam','all')
     cat.plot_galaxies('euclid','nisp','all')
@@ -112,7 +113,7 @@ def test_z11():
 
 def test_z10():
     z=10
-    cat=mock_functions.Catalogue(z=z,catalogue_path='../')
+    cat=mock_functions.Catalogue(z=z)
     cat.select_galaxies_with_indices([1])
     cat.plot_galaxies('vista','vircam','all')
     cat.plot_galaxies('euclid','nisp','all')
@@ -128,7 +129,7 @@ def test_z10():
     
 def test_z9():
     z=9
-    cat=mock_functions.Catalogue(z=z,catalogue_path='../')
+    cat=mock_functions.Catalogue(z=z)
     cat.select_galaxies_with_indices([1])
     cat.plot_galaxies('vista','vircam','all')
     cat.plot_galaxies('euclid','nisp','all')
@@ -146,7 +147,7 @@ def test_z9():
     
 def test_z8():
     z=8
-    cat=mock_functions.Catalogue(z=z,catalogue_path='../')
+    cat=mock_functions.Catalogue(z=z)
     cat.select_galaxies_with_indices([1])
     cat.plot_galaxies('vista','vircam','all')
     cat.plot_galaxies('euclid','nisp','all')
@@ -165,7 +166,7 @@ def test_z8():
 
 def test_z7():
     z=7
-    cat=mock_functions.Catalogue(z=z,catalogue_path='../')
+    cat=mock_functions.Catalogue(z=z)
     cat.select_galaxies_with_indices([1])
     # Probably over-kill to test each of the options like this, but just to be sure there are no issues
     cat.plot_galaxies('all','all','all')
